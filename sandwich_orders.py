bce60f46d42c8b987905fabd1944b30c7de7873c
Tiffany Nguyen <tnguyen94@student.cscc.edu>
2026-09-14 14:53:54 -0400

    added pastrami in the sandwich list

--- sandwich_orders.py
+++ sandwich_orders.py
@@ -3,9 +3,9 @@ finished_sandwiches = []
 
 while sandwich_orders:
     current_sandwich = sandwich_orders.pop()
-    print(f"Finished making {current_sandwich} sandwich.")
+    print(f"I made your {current_sandwich} sandwich.")
     finished_sandwiches.append(current_sandwich)
 
-print("\nFinished sandwiches:")
+print("\nThe following sandwiches are made:")
 for sandwich in finished_sandwiches:
     print(f"- {sandwich}")
